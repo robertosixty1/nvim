8921cf9dbc9c48ba86a2a1eff322b064a2e53658
robertosixty1 <roberto011herminegildo@gmail.com>
2022-10-20 17:42:44 -0300

    Update deprecated functions

--- init.lua
+++ init.lua
@@ -175,7 +175,7 @@ return require('packer').startup(function(use)
 
             -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
             local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
             local lc = require('lspconfig')
             for _, ls in ipairs(LSP_SERVERS) do
@@ -254,7 +254,7 @@ return require('packer').startup(function(use)
             })
 
             -- Set up lspconfig.
-            local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+            local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
             local lc = require('lspconfig')
             for _, ls in ipairs(LSP_SERVERS) do
